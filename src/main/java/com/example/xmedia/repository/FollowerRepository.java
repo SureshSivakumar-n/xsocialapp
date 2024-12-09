d2c396f1bc165ef652970d16c8d39471778366b5
Suresh sivakumar <sureshsivakumar@Sureshs-Laptop.local>
2024-12-09 21:14:42 +0000

    added follower repository

--- src/main/java/com/example/xmedia/repository/FollowerRepository.java
+++ src/main/java/com/example/xmedia/repository/FollowerRepository.java
@@ -3,6 +3,9 @@ package com.example.xmedia.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.xmedia.entity.Follower;
+import com.example.xmedia.entity.User;
+
 
 public interface FollowerRepository extends JpaRepository<Follower, Long> {
+    boolean existsByFollowerAndFollowee(User follower, User followee);
 }
